461ca6c451b7a457daf85da5ca50f3e7aeffac44
박찬호 <chp320@naver.com>
2019-04-02 16:46:38 +0000

    출력 피라미드 모양 변경 [출력], [응용]

--- src/cpp042.cpp
+++ src/cpp042.cpp
@@ -1,4 +1,10 @@
 /* 반복문을 이용하여 피라미드 출력하기
+[출력]
+   *
+  ***
+ *****
+*******
+[응용]
       *
     * * *
   * * * * *
@@ -12,6 +18,7 @@ int main()
 {
 	int offset = 4 ;
 	
+	/* i: 단수(1단~4단), j: 별 출력 횟수 */
 	for(int i=1, j=0; i<=offset; i++, j=0)
 	{
 		for(int k=1; k<=offset-i; k++)
@@ -19,7 +26,7 @@ int main()
 		
 		while(j != 2*i-1)
 		{
-			cout << "* " ;
+			cout << "*" ;
 			j++ ;
 		}
 		cout << endl ;
